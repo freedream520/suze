43dc4aa6d3f75357f29512b8df05dfb1eaa359c8
liqueur <liqueurize@yeah.net>
2015-03-06 20:52:29 +0800

    修改脚本

--- suze/crawl/run.py
+++ suze/crawl/run.py
@@ -10,9 +10,9 @@ import kr
 
 
 if __name__ == '__main__':
-    t1, t2 = zdrb.run()
-    print 'zdrb', t1, t2
-    t1, t2 = hx.run()
-    print 'hx', t1, t2
-    t1, t2 = kr.run()
-    print 'kr', t1, t2
+    t1, t2, v1, v2 = zdrb.run()
+    print 'zdrb', t1, t2, v1, v2
+    t1, t2, v1, v2 = hx.run()
+    print 'hx', t1, t2, v1, v2
+    t1, t2, v1, v2 = kr.run()
+    print 'kr', t1, t2, v1, v2
